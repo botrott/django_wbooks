70e4535b9414e4433f3bb8a029d80067057e3413
Alexey Petrov <padgetalex@yandex.ru>
2023-06-25 15:46:52 +0300

    edit files

--- tk_and_client/tests.py
+++ tk_and_client/tests.py
@@ -1,7 +1,7 @@
 X = [123, 20, 2000]
-Y_sphere = [7790829.84, 33493.333, 33493333333.333]
-Y_cels = [50.556, -6.667, 1093.333]
-Y_far = [253.4, 68.0, 3632.0]
+Y_SPHERE = [7790829.84, 33493.333, 33493333333.333]
+Y_CELS = [50.556, -6.667, 1093.333]
+Y_FAR = [253.4, 68.0, 3632.0]
 
 def test_calcul_sphere(x):
     result = (round(4 / 3 * 3.14 * (x ** 3), 3))
@@ -17,6 +17,6 @@ def test_convert_in_far(x):
 
 if __name__ == '__main__':
     for i in range(len(X)):
-        assert test_convert_in_cels(X[i]) in Y_cels
-        assert test_convert_in_far(X[i]) in Y_far
-        assert test_calcul_sphere(X[i]) in Y_sphere
+        assert test_convert_in_cels(X[i]) in Y_CELS
+        assert test_convert_in_far(X[i]) in Y_FAR
+        assert test_calcul_sphere(X[i]) in Y_SPHERE
